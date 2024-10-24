5921b2913e45e1c50432d8d1c80bf38e012ba4ba
Ice424 <e4elliotd@gmail.com>
2024-10-24 19:04:14 +0100

    complete?

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 '''shut up pylint'''
 import random
-import disnake
 import asyncio
-from disnake.ext import commands
 from collections import defaultdict
+import disnake
+from disnake.ext import commands
 
 intents = disnake.Intents.default()
 intents.message_content = True
@@ -26,11 +26,24 @@ class Game:
 
     def __init__(self):
         self.reset()
+        self.author = None
+        self.members = []
+        self.alive = []
+        self.nicks = []
+        self.channel = None
+        self.game_running = False
+        self.numwolves = 0
+        self.roles = defaultdict(list)  # Store player roles for easy access
+        self.votes = {}  # Store votes from players
+        self.voters = []  # List of players eligible to vote
+        self.voting_messages = []  # Store the voting messages sent to players
+        self.phase = "night"
 
     def reset(self):
         '''Resets all variables'''
         self.author = None
         self.members = []
+        self.alive = []
         self.nicks = []
         self.channel = None
         self.game_running = False
@@ -46,29 +59,36 @@ class Game:
         if member not in self.members:
             self.members.append(member)
             self.nicks.append(member.name)
-    
+            self.alive.append(member)
+
     def reset_votes(self):
         '''Resets the votes and voting messages'''
         self.votes = {}
         self.voting_messages = []
-    
+
     def remove_member(self, member):
         '''Adds a member to the game'''
-        self.members.remove(member)
+        self.alive.remove(member)
         self.nicks.remove(member.name)
+        if member in self.roles["werewolf"]:
+            self.roles["werewolf"].remove(member)
+        else:
+            self.roles["villager"].remove(member)
 
 
 game = Game()
 
 
-@bot.slash_command(name="force_stop", description="Force stops the warewolf game")
-async def force_stop(inter):
+@bot.slash_command(name="reset", description="Reset's the werewolf game DOES NOT STOP IT")
+async def reset(inter):
     game.reset()
     await inter.send("Game stopped", ephemeral=True)
 
+
 @bot.slash_command(name="test")
 async def test(inter):
-    await initiate_votes([inter.author],[1,2,3,4], 30)
+    await initiate_votes([inter.author], [1, 2, 3, 4], 30)
+
 
 @bot.slash_command(description="Start the werewolf game")
 async def start(inter, numwolves: int):
@@ -78,15 +98,19 @@ async def start(inter, numwolves: int):
         game.channel = inter.channel
         # reply's to original message to allow people to enter
         await inter.response.send_message("Press button to enter",
-                                        components=[
-                                            disnake.ui.Button(
-                                                label="Join", style=disnake.ButtonStyle.success, custom_id="join")
-                                        ],)
+                                components=[
+                                    disnake.ui.Button(
+                                        label="Join",
+                                        style=disnake.ButtonStyle.success,
+                                        custom_id="join")
+                                ],)
         # dms user with button to
         await inter.author.send("Press button to start",
                                 components=[
                                     disnake.ui.Button(
-                                        label="Start", style=disnake.ButtonStyle.success, custom_id="start")
+                                        label="Start",
+                                        style=disnake.ButtonStyle.success,
+                                        custom_id="start")
                                 ],)
     else:
         await inter.send("Sorry game has started", ephemeral=True)
@@ -117,6 +141,7 @@ async def handle_button_click(inter: disnake.MessageInteraction):
         await inter.send("The game has started!", ephemeral=True)
         await start_game()
 
+
 @bot.listen("on_dropdown")
 async def handle_dropdown_click(inter: disnake.MessageInteraction):
     if inter.data.custom_id == "voting":
@@ -140,38 +165,42 @@ async def assign_roles(members, numwolves):
     werewolves = random.sample(members, numwolves)
     for player in members:
         if player in werewolves:
-            game.roles["Werewolf"].append(player)
+            game.roles["werewolf"].append(player)
             await player.send(f"You are a werewolf. Your fellow werewolves are: {', '.join([w.name for w in werewolves if w != player])}")
         else:
-            game.roles["Villager"].append(player)
+            game.roles["villager"].append(player)
             await player.send("You are a villager")
 
 
 async def night_phase():
-    print(game.roles)
-    await initiate_votes(game.roles["Werewolf"], game.roles["Villager"], 30)
-    await day_phase()
+    game.phase = "night"
+    await initiate_votes(game.roles["werewolf"], game.roles["villager"], 30)
+    if await check_game_status():
+        await day_phase()
 
 
 async def day_phase():
-
-    await check_game_status()
+    game.phase = "day"
+    await initiate_votes(game.alive, game.alive, 100)
+    if await check_game_status():
+        await night_phase()
 
 
 async def initiate_votes(voters, choices, time):
-    game.reset_votes
+    game.reset_votes()
     voting_messages = []  # To store messages so we can delete them later
     game.voters = voters
     # Set voting message based on phase
-    msg = "Vote for a player to eliminate" if game.phase == "day" else "Vote for a player to kill"
-    
+    msg = f"Vote for a player to eliminate you have {time}s" if game.phase == "day" else f"Vote for a player to kill you have {time}s"
+
     # Send the vote message to each player and store the message object
     for player in voters:
         voting_msg = await player.send(
             msg,
             components=[
-                disnake.ui.StringSelect(custom_id="voting", 
-                                        options=[disnake.SelectOption(label=str(choice), value=str(choice)) for choice in choices])
+                disnake.ui.StringSelect(custom_id="voting",
+                                        options=[disnake.SelectOption(label=str(choice.nick),
+                                        value=str(choice.nick)) for choice in choices])
             ]
         )
         voting_messages.append(voting_msg)  # Store the message object
@@ -180,14 +209,20 @@ async def initiate_votes(voters, choices, time):
         return len(game.votes) == len(voters)
 
     # Check every second if either all votes are in or 30 seconds passed
-    for _ in range(time):
+    for i in range(time):
         if has_everyone_voted():
             break
+        if i % 5 == 0:
+            for voting_msg in voting_messages:
+                try:
+                    await voting_msg.edit(content=msg)
+                except Exception as e:
+                    print(f"Failed to edit message: {e}")
         await asyncio.sleep(1)
-
+        
     if not has_everyone_voted():
         print("Timeout: Not all players voted within 30 seconds.")
-    
+
     # Delete all voting messages after the vote has closed
     for voting_msg in voting_messages:
         try:
@@ -198,51 +233,53 @@ async def initiate_votes(voters, choices, time):
     # Process votes after the voting period ends
     await process_votes(game.votes)
 
+
 async def process_votes(votes):
     vote_counts = {}
-    
+
     for voter, choice in votes.items():
         if choice not in vote_counts:
             vote_counts[choice] = 0
         vote_counts[choice] += 1
-    
+
     if not vote_counts:
         await dmplayers(game.voters, "No votes were cast. Skipping elimination.")
         return
-    
+
     max_votes = max(vote_counts.values())
-    
-    tied_players = [player for player, count in vote_counts.items() if count == max_votes]
-    
+
+    tied_players = [player for player,
+                    count in vote_counts.items() if count == max_votes]
+
     if len(tied_players) > 1:
         await dmplayers(game.voters, "There was a tie, you may now chose between the tied contestants")
         await initiate_votes(tied_players, game.voters, 30)
     else:
-        
+
         eliminated = tied_players[0]
-        
+
         if game.phase == "night":
             await dmplayers(game.voters, f"You have slain {eliminated}")
             await game.members[game.nicks.index(eliminated)].send("The werewolf's have killed you")
         else:
             await dmplayers(game.voters, f"{eliminated} has been voted out")
             await game.members[game.nicks.index(eliminated)].send("The villager's voted you out")
-        
+
         game.remove_member(game.members[game.nicks.index(eliminated)])
 
 
-
 async def check_game_status():
-    werewolf_count = len(
-        [player for player in game.members if game.roles.get(player) == "Werewolf"])
-    villager_count = len(game.members) - werewolf_count
+    werewolf_count = len(game.roles["werewolf"])
+    villager_count = len(game.roles["villager"])
 
     if werewolf_count == 0:
         await dmplayers(game.members, "Villagers win! All werewolves have been eliminated.")
-        game.reset()
-    else:
-        # Continue to the next night phase if the game is not yet over
-        await night_phase()
+        return False
+    if villager_count == 0:
+        await dmplayers(game.members, "Werewolf's win! All villagers have been killed.")
+        return False
+
+    return True
 
 
 f = open("token.txt", encoding="utf8")
